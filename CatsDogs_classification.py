5a4e7834619de57e97b159da964fce4de6971155
KirinNg <kirinng@gmail.com>
2017-10-16 03:27:13 +0800

    add VGG19 Alex

--- CatsDogs_classification.py
+++ CatsDogs_classification.py
@@ -10,118 +10,249 @@ class Config(object):
         self.text_path = "cat&dogs/annotations/test.txt"
         self.train_path = "cat&dogs/annotations/trainval.txt"
         self.image_path = "cat&dogs/images/"
-        self.learning_rate = 0.0001
+        self.learning_rate = 0.00001
         self.num_train = 3680
-        self.iter = 1000
+        self.test_num = 3669
+        self.iter = 100000
+        self.save_path = "cat&dogs/model.ckpt"
+        self.choose = 0
+        self.wide = 227
 
 class Images(object):
     def __init__(self, config):
         self.config = config
         train_fin = open(self.config.train_path, "r")
-        self.data = np.zeros([3680,500,500,3])#
-        self.label = np.zeros([3680,37])#
+        self.data = np.zeros([self.config.num_train,self.config.wide,self.config.wide,3])#
+        self.label_0 = np.zeros([self.config.num_train,37])#
+        self.label_1 = np.zeros([self.config.num_train,2])
         i = 0
+        j = 0
         for line_train in train_fin.readlines():
-            line_train = line_train.strip().split(' ')
-            path = self.config.image_path + line_train[0] + ".jpg"
-            img = io.imread(path)
-            img = transform.resize(img,(500,500,3))
-            #io.imsave("try.jpg",img)
-            self.data[i,:,:,:] = img
-            self.label[i,int(line_train[1])-1] = 1
-            #self.label[i,1] = line_train[2]
-            #self.label[i,2] = line_train[3]
-            i += 1
+            if j%1==config.choose:
+                line_train = line_train.strip().split(' ')
+                path = self.config.image_path + line_train[0] + ".jpg"
+                img = io.imread(path)
+                img = transform.resize(img,(self.config.wide,self.config.wide,3))
+                #io.imsave("try.jpg",img)
+                self.data[i,:,:,:] = img
+                self.label_0[i,int(line_train[1])-1] = 1
+                self.label_1[i,int(line_train[2])-1] = 1
+                #self.label[i,2] = line_train[3]
+                i += 1
+                print(i)
+            j += 1
+
+class Images_test(object):
+    def __init__(self, config):
+        self.config = config
+        train_fin = open(self.config.text_path, "r")
+        self.data = np.zeros([1223,self.config.wide,self.config.wide,3])#
+        self.label_0 = np.zeros([1223,37])#
+        self.label_1 = np.zeros([1223,2])
+        i = 0
+        j = 0
+        for line_train in train_fin.readlines():
+            if j%3==config.choose:
+                line_train = line_train.strip().split(' ')
+                path = self.config.image_path + line_train[0] + ".jpg"
+                img = io.imread(path)
+                img = transform.resize(img,(self.config.wide,self.config.wide,3))
+                #io.imsave("try.jpg",img)
+                self.data[i,:,:,:] = img
+                self.label_0[i,int(line_train[1])-1] = 1
+                self.label_1[i,int(line_train[2])-1] = 1
+                #self.label[i,2] = line_train[3]
+                i += 1
+            j += 1
             print(i)
 
 class Model(object):
-    def __init__(self, config, image):
+    def __init__(self, config, image, image_test):
         self.config = config
         self.flag = 0
         #self.image = image
         ########## INPUT ##########
-        self.input = tf.placeholder(tf.float32, [None,500,500,3])
-        self.label = tf.placeholder(tf.float32, [None,37])
+        self.input = tf.placeholder(tf.float32, [None,self.config.wide,self.config.wide,3])
+        self.label_0 = tf.placeholder(tf.float32, [None,37])
+        self.label_1 = tf.placeholder(tf.float32, [None,2])
+        self.keep_prob = tf.placeholder(tf.float32)
         ###########################
         self._make_model()
         self.loss = self._make_loss()
-        self.train_step = tf.train.GradientDescentOptimizer(self.config.learning_rate).minimize(self.loss)
+        with tf.device("/gpu:0"):##########
+            self.train_step = tf.train.RMSPropOptimizer(self.config.learning_rate).minimize(self.loss)
 
     def _make_model(self):
-        def conv(input_data, input_size, output_size):
-            w = tf.Variable(tf.random_normal([3,3,input_size,output_size],stddev = 0.001))
-            b = tf.Variable(tf.constant(0.001, shape = [output_size]))
-            return tf.nn.relu(tf.nn.conv2d(input_data, w, [1,1,1,1], padding = 'SAME') + b)
+        def conv(input_data, input_size, output_size, kernel_size = 3, stride = 1):
+            w = tf.Variable(tf.random_normal([kernel_size,kernel_size,input_size,output_size],stddev = 0.01))
+            b = tf.Variable(tf.constant(0.01, shape = [output_size]))
+            return tf.nn.relu(tf.nn.conv2d(input_data, w, [1,stride,stride,1], padding = 'SAME') + b)
 
         def pooling(input_data):
-            return tf.nn.avg_pool(input_data, ksize = [1,2,2,1], strides = [1,2,2,1], padding = 'SAME')
+            return tf.nn.max_pool(input_data, ksize = [1,3,3,1], strides = [1,2,2,1], padding = 'VALID')
 
         def addLayer(input_data, input_size, output_size):
-            W = tf.Variable(tf.random_normal([input_size,output_size],stddev = 0.001))
-            basis = tf.Variable(tf.constant(0.001,shape = [output_size]))
+            W = tf.Variable(tf.random_normal([input_size,output_size],stddev = 0.01))
+            basis = tf.Variable(tf.constant(0.01,shape = [output_size]))
             return tf.matmul(input_data, W) + basis
-
-        #500*500*3
+        """
+        #128*128*3
         lay1 = conv(self.input, 3, 4)
         lay1_p = pooling(lay1)
-        #250*250*4
+        #64*64*4
         lay2 = conv(lay1_p, 4, 16)
         lay2_p = pooling(lay2)
-        #125*125*16
+        #32*32*16
         lay3 = conv(lay2_p, 16, 64)
         lay3_p = pooling(lay3)
-        #63*63*64
+        #16*16*64
         lay4 = conv(lay3_p, 64, 256)
         lay4_p = pooling(lay4)
-        #32*32*256
-        lay5 = conv(lay4_p, 256, 256)
-        lay5_p = pooling(lay5)
-        #16*16*256
-        lay6 = conv(lay5_p, 256, 256)
-        lay6_p = pooling(lay6)
         #8*8*256
-        lay7 = conv(lay6_p, 256, 256)
-        lay7_p = pooling(lay7)
+        lay5 = conv(lay4_p, 256, 512)
+        lay5_p = pooling(lay5)
         #4*4*256
-        array7 = tf.reshape(lay7_p, [-1,4096])
-        #4096
+        lay6 = conv(lay5_p, 512, 1024)
+        lay6_p = pooling(lay6)
+        #2*2*256
+        lay7 = conv(lay6_p, 1024, 1024)
+        lay7_p = pooling(lay7)
+        #1*1*256
+        array7 = tf.reshape(lay7_p, [-1,1024])
+        #1024
         #out_put
-        array8 = tf.nn.relu(addLayer(array7, 4096, 1024))
-        self.output = tf.nn.softmax(addLayer(array8, 1024, 37))
-
+        array8 = tf.nn.relu(addLayer(array7, 1024, 256))
+        self.output = tf.nn.dropout(addLayer(array8, 256, 37), self.keep_prob)
+        """
+        
+        #Alex_Net 
+        #DFD_1 227*227*3
+        lay1 = conv(self.input,3,96,11,4)
+        lay1_p = pooling(lay1)
+        #DFD_2 27*27*96
+        lay2 = conv(lay1_p,96,256,5,1)
+        lay2_p = pooling(lay2)
+        #DFD_3 13*13*256
+        lay3 = conv(lay2_p,256,384,3,1)
+        #DFD_4 13*13*384
+        lay4 = conv(lay3,384,384,3,1)
+        #DFD_5 13*13*384
+        lay5 = conv(lay4,384,256,3,1)
+        lay5_p = pooling(lay5)
+        #DFD_6 6*6*256
+        array6 = tf.reshape(lay5_p,[-1,9216])
+        fc6 = tf.nn.relu(addLayer(array6,9216,4096))
+        drop6 = tf.nn.dropout(fc6,self.keep_prob)
+        #DFD_7 -1*4096
+        fc7 = tf.nn.relu(addLayer(drop6,4096,4096))
+        drop7 = tf.nn.dropout(fc7,self.keep_prob)
+        #DFD_8 -1*4096
+        self.output = addLayer(drop7,4096,39)
+        #FINISH
+        """
+        #VGG_19 224*224*3
+        lay1 = conv(self.input, 3, 64)
+        lay2 = pooling(conv(lay1, 64, 64))
+        #112*112*64
+        lay3 = conv(lay2, 64,128)
+        lay4 = pooling(conv(lay3, 128, 128))
+        #56*56*128
+        lay5 = conv(lay4, 128, 256)
+        lay6 = conv(lay5, 256, 256)
+        lay7 = conv(lay6, 256, 256)
+        lay8 = pooling(conv(lay7, 256, 256))
+        #28*28*256
+        lay9 = conv(lay8, 256, 512)
+        lay10 = conv(lay9, 512, 512)
+        lay11 = conv(lay10, 512, 512)
+        lay12 = pooling(conv(lay11, 512, 512))
+        #14*14*512
+        lay13 = conv(lay12, 512 ,512)
+        lay14 = conv(lay13, 512, 512)
+        lay15 = conv(lay14, 512, 512)
+        lay16 = pooling(conv(lay15, 512, 512))
+        #7*7*512
+        array16 = tf.reshape(lay16,[-1,7*7*512])
+        lay17 = tf.nn.dropout(addLayer(array16, 7*7*512, 4096),self.keep_prob)
+        lay18 = tf.nn.dropout(addLayer(lay17, 4096, 4096),self.keep_prob)
+        lay19 = addLayer(lay18, 4096, 1000)
+        self.output = addLayer(lay19, 1000, 39)
+        """
     def _make_loss(self):
         def loss(y_out, ys):
             return tf.reduce_sum(tf.square(y_out-ys))
         def logic_loss(y_out, ys):
             return -tf.reduce_sum(ys*tf.log(y_out))
-        return logic_loss(self.output, self.label)
+        def tfsoft_with_entrance(y_out, ys):
+            return tf.nn.softmax_cross_entropy_with_logits(None,ys,y_out)
+        return tf.reduce_sum(tfsoft_with_entrance(self.output[:,0:37], self.label_0)) + 16*tf.reduce_sum(tfsoft_with_entrance(self.output[:,37:39], self.label_1))
 
     def _get_feed(self):
-        self.min_batch_data = image.data[self.flag:self.flag+92,:,:,:]
-        self.min_batch_label = image.label[self.flag:self.flag+92,:]
-        self.flag += 92
-        if self.flag == 3680:
-            self.flag = 0
-        return {self.input:self.min_batch_data, self.label:self.min_batch_label}
+        self.min_batch_data = image.data[self.flag*368:self.flag*368+368,:,:,:]
+        self.min_batch_label_0 = image.label_0[self.flag*368:self.flag*368+368,:]
+        self.min_batch_label_1 = image.label_1[self.flag*368:self.flag*368+368,:]
+        self.flag += 1
+        self.flag = self.flag%10
+        return {self.input:self.min_batch_data, self.label_1:self.min_batch_label_1, self.label_0:self.min_batch_label_0, self.keep_prob:0.5}
 
     def run_model(self):
-        config = tf.ConfigProto()
+        #config = tf.ConfigProto()
+        config = tf.ConfigProto(allow_soft_placement=True)
+        config.gpu_options.allocator_type = 'BFC'
+        #config.gpu_options.per_process_gpu_memory_fraction = 0.40
         config.gpu_options.allow_growth = True
         self.sess = tf.Session(config=config)
         self.sess.run(tf.global_variables_initializer())
-        correct_p = tf.equal(tf.argmax(self.output,1),(tf.argmax(self.label,1)))
+        ###
+        self.restore_model(self.config.save_path)
+        ###
+        correct_p = tf.equal(tf.argmax(self.output[:,0:37],1),(tf.argmax(self.label_0,1)))
         accuracy = tf.reduce_mean(tf.cast(correct_p, "float"))
         for i in range(self.config.iter):
-            for j in range(40):
-                if i>300:
-                    self.config.learning_rate = 0.00001
+            for j in range(10):
+                #if i>100:
+                #    self.config.learning_rate = 0.00001
+                if i>500:
+                    self.config.learning_rate = 0.0000001
                 print()
-                feed = self._get_feed()
-                self.sess.run(self.train_step, feed)
-                print("i:"+str(i)+" j:"+str(j)+" loss"+str(self.sess.run(self.loss, feed))+" accur"+str(self.sess.run(accuracy, feed)))
+                with tf.device("/cpu:0"):##########
+                    feed = self._get_feed()
+                    feed_test = {self.input:image_test.data, self.label_1:image_test.label_1, self.label_0:image_test.label_0, self.keep_prob:1}
+                    self.sess.run(self.train_step, feed)
+                with tf.device("/cpu:0"):
+                    print("i:%d j:%d loss:%.6f accur:%f accur_test:%f" %(i,j,self.sess.run(self.loss, feed),self.sess.run(accuracy, feed),self.sess.run(accuracy, feed_test)))
+                    #print("loss:%.6f accur:%f" %(self.sess.run(self.loss, feed),self.sess.run(accuracy, feed)))
+                    #print("accur_test:%f" %(self.sess.run(accuracy, feed_test)))
+            if i%100 == 0:
+                if i>0:
+                    self.save(self.config.save_path)
+
+    def save(self, path):
+        saver = tf.train.Saver()
+        saver.save(self.sess, path)
+
+    def restore_model(self, path):
+        saver = tf.train.Saver()
+        saver.restore(self.sess, path)
+
+    def test(self):
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+        correct_p = tf.equal(tf.argmax(self.output[:,0:37],1),(tf.argmax(self.label_1,1)))
+        accuracy = tf.reduce_mean(tf.cast(correct_p, "float"))
+        self.restore_model(self.config.save_path)
+        accur = 0
+        for i in range(3669):
+            feed_test = {self.input:image_test.data[i:i+1,:,:,:], self.label_1:image_test.label_1[i:i+1,:], self.label_0:image_test.label_0[i:i+1,:], self.keep_prob:1}
+            accur += self.sess.run(accuracy, feed_test)
+            print(i)
+        print("accur%f" %(accur/3669))
 
 # MAIN 部分
 config = Config()
 image = Images(config)
-model = Model(config, image)
+image_test = Images_test(config)
+model = Model(config, image, image_test)
 model.run_model()
+model.save(config.save_path)
+model.test()
